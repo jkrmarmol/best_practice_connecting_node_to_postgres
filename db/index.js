1b46a700e80143a5c3cc86e3629212da4890baae
Kurt Russelle Marmol <jkurtrussellemarmol@gmail.com>
2022-07-17 13:15:30 +0800

    new b

--- db/index.js
+++ db/index.js
@@ -1,10 +1,17 @@
 const Pool = require('pg').Pool;
-const pool = new Pool({
-    user: 'postgres',
-    hostname: 'localhost',
-    database: 'best_practice',
-    password: 'postgres',
-    port: 5432
-});
+// const pool = new Pool({
+//     user: 'postgres',
+//     hostname: 'localhost',
+//     database: 'best_practice',
+//     password: 'postgres',
+//     port: 5432
+// });
+
+const pool = new Pool({ // create connection to database
+    connectionString: process.env.DATABASE_URL,	// use DATABASE_URL environment variable from Heroku app 
+    ssl: {
+      rejectUnauthorized: false // don't check for SSL cert
+    }
+  });
 
 module.exports = pool;
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 80;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
